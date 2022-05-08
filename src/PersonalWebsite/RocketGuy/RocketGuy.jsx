585943701f28e1097988120fef7464c2a641e023
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-05-08 07:24:15 -0500

    created rocket guy files and reorganized animation sprites

---
+++ src/PersonalWebsite/RocketGuy/RocketGuy.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './RocketGuy.css';
+
+export default class RocketGuy extends React.Component {
+    render() {
+        return (
+            <div id='rocket-guy'></div>
+        );
+    }
+}
