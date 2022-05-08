9a8240637110c87af7f0df2f54380800c55014fb
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-05-08 08:52:03 -0500

    Added rocket guy fly up animations

--- src/PersonalWebsite/RocketGuy/RocketGuy.jsx
+++ src/PersonalWebsite/RocketGuy/RocketGuy.jsx
@@ -2,6 +2,29 @@ import React from 'react';
 import './RocketGuy.css';
 
 export default class RocketGuy extends React.Component {
+    componentDidMount() {
+        this.switchToIdleAnimation();
+        // this.swtichToFlyUpBuildAnimation();
+        // this.swtichToFlyUpSustainAnimation();
+        // this.swtichToFlyUpStopAnimation();
+    }
+
+    switchToIdleAnimation() {
+        document.querySelector('#rocket-guy').classList.add('idle');
+    }
+
+    swtichToFlyUpBuildAnimation() {
+        document.querySelector('#rocket-guy').classList.add('fly-up-build');
+    }
+
+    swtichToFlyUpSustainAnimation() {
+        document.querySelector('#rocket-guy').classList.add('fly-up-sustain');
+    }
+
+    swtichToFlyUpStopAnimation() {
+        document.querySelector('#rocket-guy').classList.add('fly-up-stop');
+    }
+
     render() {
         return (
             <div className='sprite' id='rocket-guy'></div>
