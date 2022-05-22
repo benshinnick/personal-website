a324f146512b330b8a4f23401f3d06fd2eb1ade6
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-05-22 16:07:06 -0500

    Implemented infinite scrolling cloud effect

--- src/PersonalWebsite/Clouds/Clouds.jsx
+++ src/PersonalWebsite/Clouds/Clouds.jsx
@@ -4,9 +4,14 @@ import './Clouds.css';
 export default class Clouds extends React.Component {
     render() {
         return (
-            <div>
-                <canvas className='sprite medium-cloud'></canvas>
-                <canvas className='sprite medium-large-cloud'></canvas>
+            <div id='clouds'>
+                <canvas className='sprite cloud large' id='large-1'></canvas>
+                <canvas className='sprite cloud large' id='large-2'></canvas>
+                <canvas className='sprite cloud medium' id='medium-1'></canvas>
+                <canvas className='sprite cloud medium' id='medium-2'></canvas>
+                <canvas className='sprite cloud small' id='small-1'></canvas>
+                <canvas className='sprite cloud small' id='small-2'></canvas>
+                <canvas className='sprite cloud small' id='small-3'></canvas>
             </div>
         );
     }
