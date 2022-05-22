8bcc89e289667e37150bc60576824458fd91da12
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-05-22 12:41:16 -0500

    Created and added medium large cloud image

--- src/PersonalWebsite/Clouds/Clouds.jsx
+++ src/PersonalWebsite/Clouds/Clouds.jsx
@@ -4,7 +4,10 @@ import './Clouds.css';
 export default class Clouds extends React.Component {
     render() {
         return (
-            <canvas className='sprite medium-cloud'></canvas>
+            <div>
+                <canvas className='sprite medium-cloud'></canvas>
+                <canvas className='sprite medium-large-cloud'></canvas>
+            </div>
         );
     }
 }
