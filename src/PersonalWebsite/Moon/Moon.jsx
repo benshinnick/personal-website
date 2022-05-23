b65a7195b3060bec4fe91fa834d6a174d3074afd
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-05-22 19:12:51 -0500

    Added background moon

---
+++ src/PersonalWebsite/Moon/Moon.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Moon.css';
+
+export default class Moon extends React.Component {
+    render() {
+        return (
+            <div id='moon'></div>
+        );
+    }
+}
