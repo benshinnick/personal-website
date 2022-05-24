ebaba07a830be9f39e740b6075e08af59bba3cc1
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-05-23 20:20:14 -0500

    Updated moon and navbar styling

---
+++ src/PersonalWebsite/AboutPanel/AboutPanel.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Clouds.css';
+
+export default class Clouds extends React.Component {
+    render() {
+        return (
+            <div id='clouds'></div>
+        );
+    }
+}
