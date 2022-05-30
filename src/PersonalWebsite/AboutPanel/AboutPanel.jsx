3a14f2e1fec37340cf53d5ba65d4d7893dcc9d54
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-05-30 13:55:30 -0500

    Updated background gradient and page layout

--- src/PersonalWebsite/AboutPanel/AboutPanel.jsx
+++ src/PersonalWebsite/AboutPanel/AboutPanel.jsx
@@ -7,26 +7,33 @@ export default class AboutPanel extends React.Component {
             <div className='section'>
                 <div className='container'>
                     <h2 id='greeting-text'>Hi, I'm Ben!</h2>
-                    <h2 id='about-text'>
-                    QUICK INTRODUCTION:<br></br>
-                    - STUDENT<br></br>
-                    - DEVELOPER<br></br>
-                    - GOALS<br></br>
-                    - INTERESTS<br></br>
-                    <br></br>
-                    EDUCATION:<br></br>
-                    - University Of West Florida<br></br>
-                    - B.S. in Computer Science<br></br>
-                    - 2020 - 2023<br></br>
-                    </h2>
-                    <div id='about-panel'>
+                    <div class='about-panel'>
                         <div className='corner' id='top-right-corner'></div>
                         <div className='corner' id='bottom-right-corner'></div>
                         <div className='corner' id='bottom-left-corner'></div>
                         <div className='corner' id='top-left-corner'></div>
                         <div id='right-side'></div>
                         <div id='left-side'></div>
-                        <div id="info-panel"></div>
+                        <div id='info-panel'></div>
+                        <h2 id='about-text'>
+                        QUICK INTRODUCTION:<br></br>
+                        - STUDENT<br></br>
+                        - DEVELOPER<br></br>
+                        - GOALS<br></br>
+                        - INTERESTS<br></br>
+                        <br></br>
+                        EDUCATION:<br></br>
+                        - CS @ UWF
+                    </h2>
+                    </div>
+                    <div class='connect-panel'>
+                        <div className='corner' id='top-right-corner'></div>
+                        <div className='corner' id='bottom-right-corner'></div>
+                        <div className='corner' id='bottom-left-corner'></div>
+                        <div className='corner' id='top-left-corner'></div>
+                        <div id='right-side'></div>
+                        <div id='left-side'></div>
+                        <div id='info-panel'></div>
                     </div>
                 </div>
             </div>
