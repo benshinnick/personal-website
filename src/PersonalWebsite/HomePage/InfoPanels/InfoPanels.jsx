1fe570459fe89ec4f6b17eb31b9b3f9b6308c0db
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-06-16 20:48:36 -0500

    Finished connect panel underline transitions

--- src/PersonalWebsite/HomePage/InfoPanels/InfoPanels.jsx
+++ src/PersonalWebsite/HomePage/InfoPanels/InfoPanels.jsx
@@ -12,31 +12,29 @@ export default class InfoPanels extends React.Component {
         else { document.getElementById('connect-info').className = 'default' }
         document.getElementById('connect-text').className = 'black-text'
         document.getElementById('underline').className = 'down'
-        document.getElementById('underline').style.bottom = '15px'
-        // document.getElementById('connect-text').textContent = 'CONNECT WITH ME'
+        document.getElementById('underline').style.bottom = '13px'
+        document.getElementById('underline').style.transform = 'scale(0, 1)';
     }
 
     changeConnectInfoToGithub() {
         document.getElementById('connect-info').className = 'github'
         document.getElementById('connect-text').className = 'white-text'
-        document.getElementById('underline').style.bottom = '20px'
-        // document.getElementById('connect-text').textContent = 'CONNECT WITH ME\r\nON GITHUB'
+        document.getElementById('underline').style.bottom = '18px'
+        document.getElementById('underline').style.transform = 'scale(1, 1)';
     }
 
     changeConnectInfoToLinkedIn() {
         document.getElementById('connect-info').className = 'linked-in'
         document.getElementById('connect-text').className = 'white-text'
-        document.getElementById('underline').style.bottom = '20px'
-        // document.getElementById('connect-text').textContent = 'CONNECT WITH ME\r\nON LINKED IN'
+        document.getElementById('underline').style.bottom = '18px'
+        document.getElementById('underline').style.transform = 'scale(1, 1)';
     }
 
     changeConnectInfoToEmail() {
         document.getElementById('connect-info').className = 'email'
         document.getElementById('connect-text').className = 'white-text'
-        document.getElementById('underline').style.bottom = '20px'
-        document.getElementById('underline').style.opacity = '1';
-        document.getElementById('underline').style.transform = 'translate3d(0, 0.2em, 0)';
-        // document.getElementById('connect-text').textContent = 'CONNECT WITH ME\r\nBY EMAIL'
+        document.getElementById('underline').style.bottom = '18px'
+        document.getElementById('underline').style.transform = 'scale(1, 1)';
     }
 
     render() {
