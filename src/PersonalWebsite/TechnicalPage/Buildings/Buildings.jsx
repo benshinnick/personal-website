9ac6d671b07813d7d264ebb083caa1febadef579
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-07-31 18:20:19 -0500

    Added building 4

--- src/PersonalWebsite/TechnicalPage/Buildings/Buildings.jsx
+++ src/PersonalWebsite/TechnicalPage/Buildings/Buildings.jsx
@@ -5,9 +5,12 @@ export default class Buildings extends React.Component {
     render() {
         return (
             <div>
-                <div id='building-1' className='sprite'></div>
-                <div id='building-2' className='sprite'></div>
-                <div id='building-3' className='sprite'></div>
+                <div id='right-buildings'>
+                    <div id='building-1' className='sprite'></div>
+                    <div id='building-2' className='sprite'></div>
+                    <div id='building-3' className='sprite'></div>
+                </div>
+                <div id='building-4' className='sprite'></div>
             </div>
         );
     }
