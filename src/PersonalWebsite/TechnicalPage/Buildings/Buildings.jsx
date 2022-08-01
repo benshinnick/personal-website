7f956435828b3499680019e6d9daece00ac4811c
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-07-31 22:00:46 -0500

    Separated top and floor building sprites for scalable heights

--- src/PersonalWebsite/TechnicalPage/Buildings/Buildings.jsx
+++ src/PersonalWebsite/TechnicalPage/Buildings/Buildings.jsx
@@ -6,11 +6,23 @@ export default class Buildings extends React.Component {
         return (
             <div>
                 <div id='right-buildings'>
-                    <div id='building-1' className='sprite'></div>
-                    <div id='building-2' className='sprite'></div>
-                    <div id='building-3' className='sprite'></div>
+                    <div id='building-1'>
+                        <div id='building-1-top' className='sprite'></div>
+                        <div id='building-1-floors' className='sprite'></div>
+                    </div>
+                    <div id='building-2'>
+                        <div id='building-2-top' className='sprite'></div>
+                        <div id='building-2-floors' className='sprite'></div>
+                    </div>
+                    <div id='building-3'>
+                        <div id='building-3-top' className='sprite'></div>
+                        <div id='building-3-floors' className='sprite'></div>
+                    </div>
                 </div>
-                <div id='building-4' className='sprite'></div>
+                <div id='building-4'>
+                    <div id='building-4-top' className='sprite'></div>
+                    <div id='building-4-floors' className='sprite'></div>
+                </div>
             </div>
         );
     }
--- src/PersonalWebsite/TechnicalPage/TechnicalPage.jsx
+++ src/PersonalWebsite/TechnicalPage/TechnicalPage.jsx
@@ -3,11 +3,23 @@ import './TechnicalPage.css';
 import Buildings from './Buildings/Buildings';
 
 export default class TechnicalPage extends React.Component {
+    componentDidMount() {
+        window.addEventListener('resize', this.handleResize)
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleResize)
+    }
+
+    handleResize() {
+        document.getElementById('filler-tech').style.height = `${Math.floor(window.innerHeight*8.5)}px`
+    }
+
     render() {
         return (
             <main className='technical-page'>
                 <Buildings />
-                <div id='filler' style={{height: window.innerHeight*3.3 + 'px'}} />
+                <div id='filler-tech' style={{height: window.innerHeight*8.5 + 'px'}} />
             </main>
         );
     }
