4405726af5e58d583840af26a8cfc3a2999647f0
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-08-02 20:45:36 -0500

    Rearanged stars on home page and added stars to technical page that are commented out for now

--- src/PersonalWebsite/TechnicalPage/TechnicalPage.jsx
+++ src/PersonalWebsite/TechnicalPage/TechnicalPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './TechnicalPage.css';
 import Moon from './Moon/Moon';
 import Buildings from './Buildings/Buildings';
+// import Stars from './Stars/Stars'
 
 export default class TechnicalPage extends React.Component {
     constructor(props) {
@@ -32,6 +33,7 @@ export default class TechnicalPage extends React.Component {
             <main className='technical-page'>
                 <Buildings ref={this.buildingsRef} />
                 <Moon />
+                {/* <Stars /> */}
                 <div id='filler-tech' style={{height: window.innerHeight*8.5 + 'px'}} />
             </main>
         );
