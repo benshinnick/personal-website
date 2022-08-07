47ef6a686fed8379e8e4876616558e1b7e228872
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-08-06 23:24:20 -0500

    Implemented tech info panels cover each other on transition

--- src/PersonalWebsite/TechnicalPage/InfoPanels/TechInfoPanels.jsx
+++ src/PersonalWebsite/TechnicalPage/InfoPanels/TechInfoPanels.jsx
@@ -6,21 +6,31 @@ var lastScrollPos = 0
 var currentPanelFocus
 
 var aboutPanelMaxHeight
+var aboutPanelCoverHeight
 var aboutPanelHeight
+var aboutPanelOpacity
+
 var projectsPanelMaxHeight
 var projectsPanelHeight
+var projectsPanelOpacity
+var projectsPanelTopMargin
 
 export default class TechInfoPanels extends React.Component {
 
     componentDidMount() {
         currentPanelFocus = 'about'
+
         aboutPanelHeight = document.getElementById('about-panel-content').offsetHeight + 6
         aboutPanelMaxHeight = aboutPanelHeight
         document.getElementById('about-panel-content-container').style.height = `${aboutPanelHeight}px`
+        aboutPanelOpacity = 1
+        aboutPanelCoverHeight = document.getElementById('about-title').clientHeight + 28
 
         projectsPanelHeight = document.getElementById('projects-panel-content').offsetHeight + 6
         projectsPanelMaxHeight = projectsPanelHeight
         document.getElementById('projects-panel-content-container').style.height = `${projectsPanelHeight}px`
+        projectsPanelOpacity = 1
+        projectsPanelTopMargin = 0
     }
 
     onScroll() {
@@ -35,7 +45,25 @@ export default class TechInfoPanels extends React.Component {
             }
             else if (aboutPanelHeight < 0) {
                 aboutPanelHeight = 0
-                currentPanelFocus = 'projects'
+                projectsPanelTopMargin -= scrollDiff
+                aboutPanelOpacity -= scrollDiff / 50
+                if (Math.abs(projectsPanelTopMargin) >= aboutPanelCoverHeight) {
+                    projectsPanelTopMargin = -1 * aboutPanelCoverHeight
+                    currentPanelFocus = 'projects'
+                }
+                if (aboutPanelOpacity <= 0) { aboutPanelOpacity = 0 }
+                document.getElementById('tech-projects-panel').style.marginTop = `${projectsPanelTopMargin}px`
+                document.getElementById('tech-about-panel').style.opacity = aboutPanelOpacity
+            }
+            else if (projectsPanelTopMargin < 0) {
+                projectsPanelTopMargin -= scrollDiff
+                aboutPanelOpacity -= scrollDiff / 50
+                if (projectsPanelTopMargin >= 0) {
+                    projectsPanelTopMargin = 0
+                }
+                if (aboutPanelOpacity >= 1) { aboutPanelOpacity = 1 }
+                document.getElementById('tech-projects-panel').style.marginTop = `${projectsPanelTopMargin}px`
+                document.getElementById('tech-about-panel').style.opacity = aboutPanelOpacity
             }
             document.getElementById('about-panel-content-container').style.height = `${aboutPanelHeight}px`
         }
@@ -59,20 +87,20 @@ export default class TechInfoPanels extends React.Component {
             <div className='section'>
                 <div className='container'>
                     <div id='technical-panels'>
-                        <div className='tech-text-panel'>
+                        <div className='tech-info-panel' id='tech-about-panel'>
                             <div className='corner' id='top-right-corner'></div>
                             <div className='corner' id='bottom-right-corner'></div>
                             <div className='corner' id='bottom-left-corner'></div>
                             <div className='corner' id='top-left-corner'></div>
                             <div className='sides'></div>
-                            <div className='title'>ABOUT</div>
+                            <div className='title' id='about-title'>ABOUT</div>
                             <div className='info-panel-content-container' id='about-panel-content-container'>
                                 <div className='info-panel-content' id='about-panel-content'>
                                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id ornare velit. Donec mollis vitae turpis non laoreet. Maecenas ac mauris non ante egestas tempus. Donec nisl enim, elementum non mauris sed, cursus dictum nisl. Nunc sed tortor pellentesque, elementum nulla id, ullamcorper urna. Curabitur nisi sem.
                                 </div>
                             </div>
                         </div>
-                        <div className='tech-text-panel'>
+                        <div className='tech-info-panel' id='tech-projects-panel'>
                             <div className='corner' id='top-right-corner'></div>
                             <div className='corner' id='bottom-right-corner'></div>
                             <div className='corner' id='bottom-left-corner'></div>
