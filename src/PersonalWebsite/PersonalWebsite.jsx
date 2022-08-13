ec8df3e605b0e319e670e820ca7823bc7a6a7cbc
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-08-13 13:58:10 -0500

    Changed some page transition timings

--- src/PersonalWebsite/PersonalWebsite.jsx
+++ src/PersonalWebsite/PersonalWebsite.jsx
@@ -67,7 +67,7 @@ export default class PersonalWebsite extends React.Component {
                 this.cloudsRef.current.transitionCloudsToBottom()
                 this.navBarRef.current.switchToAbout()
                 const mainContent = document.getElementById('main-content')
-                mainContent.style.animation = '750ms home-disappear forwards'
+                mainContent.style.animation = '500ms home-disappear forwards'
                 this.navBarRef.current.transitionToHome()
                 setTimeout(() => {
                     root.render(<HomePage />)
@@ -88,7 +88,7 @@ export default class PersonalWebsite extends React.Component {
                 this.cloudsRef.current.transitionCloudsToTop()
                 this.navBarRef.current.switchToTechnical()
                 const mainContent = document.getElementById('main-content')
-                mainContent.style.animation = '750ms home-disappear forwards'
+                mainContent.style.animation = '500ms home-disappear forwards'
                 setTimeout(() => {
                     this.navBarRef.current.transitionToOverCloud()
                     root.render(<TechnicalPage />)
