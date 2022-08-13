b18f4ef4b1e74b081561ce373bf441c408285f28
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-08-13 18:35:03 -0500

    Added project icons to technical page

--- src/PersonalWebsite/TechnicalPage/InfoPanels/ProjectsPanel/ProjectsPanel.jsx
+++ src/PersonalWebsite/TechnicalPage/InfoPanels/ProjectsPanel/ProjectsPanel.jsx
@@ -6,6 +6,12 @@ export default class ProjectsPanel extends React.Component {
     render() {
         return (
             <div className='info-panel-content' id='projects-panel-content'>
+                <div id='project-icons'>
+                    <div className='sprite project-icon' id='algo-contest-icon'/>
+                    <div className='sprite project-icon' id='huddle-icon'/>
+                    <div className='sprite project-icon' id='mystery-icon'/>
+                    <div className='sprite project-icon' id='mystery-icon'/>
+                </div>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id ornare velit. Donec mollis vitae turpis non laoreet. Maecenas ac mauris non ante egestas tempus. Donec nisl enim, elementum non mauris sed, cursus dictum nisl. Nunc sed tortor pellentesque, elementum nulla id, ullamcorper urna. Curabitur nisi sem, tristique sed velit porta, molestie vulputate sem. Cras non tincidunt sapien. Nunc rhoncus tempus risus, at ornare sem interdum at. Donec rhoncus tortor justo. Praesent dignissim turpis pretium, volutpat sapien vitae, dapibus velit.
             </div>
         );
