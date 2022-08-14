59ee7ab3761f8397c168050079e2ef369f3385ab
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-08-14 09:15:41 -0500

    Added personal website icon to projects panel

--- src/PersonalWebsite/TechnicalPage/InfoPanels/ProjectsPanel/ProjectsPanel.jsx
+++ src/PersonalWebsite/TechnicalPage/InfoPanels/ProjectsPanel/ProjectsPanel.jsx
@@ -10,7 +10,7 @@ export default class ProjectsPanel extends React.Component {
                     <div className='sprite project-icon' id='algo-contest-icon'/>
                     <div className='sprite project-icon' id='huddle-icon'/>
                     <div className='sprite project-icon' id='mystery-icon'/>
-                    <div className='sprite project-icon' id='mystery-icon'/>
+                    <div className='sprite project-icon' id='personal-website-icon'/>
                 </div>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id ornare velit. Donec mollis vitae turpis non laoreet. Maecenas ac mauris non ante egestas tempus. Donec nisl enim, elementum non mauris sed, cursus dictum nisl. Nunc sed tortor pellentesque, elementum nulla id, ullamcorper urna. Curabitur nisi sem, tristique sed velit porta, molestie vulputate sem. Cras non tincidunt sapien. Nunc rhoncus tempus risus, at ornare sem interdum at. Donec rhoncus tortor justo. Praesent dignissim turpis pretium, volutpat sapien vitae, dapibus velit.
             </div>
