4a05bf022d70d4db40bc001ea2018cb5b6f306ed
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-08-15 11:29:27 -0500

    Project displays  are now selectable and unselectable

--- src/PersonalWebsite/TechnicalPage/InfoPanels/ProjectsPanel/ProjectsPanel.jsx
+++ src/PersonalWebsite/TechnicalPage/InfoPanels/ProjectsPanel/ProjectsPanel.jsx
@@ -3,27 +3,37 @@ import './ProjectsPanel.css';
 
 export default class ProjectsPanel extends React.Component {
 
+    projectDisplayOnClick(type) {
+        let selectedElem = document.querySelector('.proj-selected')
+        if(selectedElem === null) {
+            document.getElementById(`${type}-display`).classList.add('proj-selected')
+            return
+        }
+        selectedElem.className = 'project-display'
+        if(selectedElem.id !== `${type}-display`) {
+            document.getElementById(`${type}-display`).classList.add('proj-selected')
+        }
+    }
     render() {
         return (
             <div className='info-panel-content' id='projects-panel-content'>
                 <div id='project-icons'>
-                    <div className='project-display selected' id='algo-contest-display' onClick={() => console.log('Algo Contest')}>
+                    <div className='project-display' id='algo-contest-display' onClick={() => this.projectDisplayOnClick('algo-contest')}>
                         <div id='algo-contest-icon' className='sprite project-icon' />
                         <div className='project-text'>AlgoContest</div>
                     </div>
-                    <div className='project-display' id='huddle-display' onClick={() => console.log('Huddle')}>
+                    <div className='project-display' id='huddle-display' onClick={() => this.projectDisplayOnClick('huddle')}>
                         <div className='sprite project-icon' id='huddle-icon' />
                         <div className='project-text'>Huddle</div>
                     </div>
-                    <div className='project-display' id='miscellaneous-display' onClick={() => console.log('Misc.')}>
+                    <div className='project-display' id='miscellaneous-display' onClick={() => this.projectDisplayOnClick('miscellaneous')}>
                         <div className='sprite project-icon' id='miscellaneous-icon' />
                         <div className='project-text'>Miscellaneous</div>
                     </div>
-                    <div className='project-display' id='personal-website-display' onClick={() => console.log('Personal')}>
+                    <div className='project-display' id='personal-website-display' onClick={() => this.projectDisplayOnClick('personal-website')}>
                         <div className='sprite project-icon' id='personal-website-icon' />
                         <div className='project-text'>Personal Site</div>
                     </div>
-                    {/* <div className='sprite project-icon' id='mystery-icon'/> */}
                 </div>
                 <div id='project-info-content'>
                     {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id ornare velit. Donec mollis vitae turpis non laoreet. Maecenas ac mauris non ante egestas tempus. Donec nisl enim, elementum non mauris sed, cursus dictum nisl. Nunc sed tortor pellentesque, elementum nulla id, ullamcorper urna. Curabitur nisi sem, tristique sed velit porta, molestie vulputate sem. Cras non tincidunt sapien. Nunc rhoncus tempus risus, at ornare sem interdum at. Donec rhoncus tortor justo. Praesent dignissim turpis pretium, volutpat sapien vitae, dapibus velit. */}
