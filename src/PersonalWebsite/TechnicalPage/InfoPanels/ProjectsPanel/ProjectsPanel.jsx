ff7cbf82e8ea8cf2a97e59678cd7723393f92c2f
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-08-15 10:50:08 -0500

    Added color backgrounds and clickablility to project displays

--- src/PersonalWebsite/TechnicalPage/InfoPanels/ProjectsPanel/ProjectsPanel.jsx
+++ src/PersonalWebsite/TechnicalPage/InfoPanels/ProjectsPanel/ProjectsPanel.jsx
@@ -7,25 +7,27 @@ export default class ProjectsPanel extends React.Component {
         return (
             <div className='info-panel-content' id='projects-panel-content'>
                 <div id='project-icons'>
-                    <div className='project-display'>
+                    <div className='project-display selected' id='algo-contest-display' onClick={() => console.log('Algo Contest')}>
                         <div id='algo-contest-icon' className='sprite project-icon' />
                         <div className='project-text'>AlgoContest</div>
                     </div>
-                    <div className='project-display'>
+                    <div className='project-display' id='huddle-display' onClick={() => console.log('Huddle')}>
                         <div className='sprite project-icon' id='huddle-icon' />
                         <div className='project-text'>Huddle</div>
                     </div>
-                    <div className='project-display'>
+                    <div className='project-display' id='miscellaneous-display' onClick={() => console.log('Misc.')}>
                         <div className='sprite project-icon' id='miscellaneous-icon' />
                         <div className='project-text'>Miscellaneous</div>
                     </div>
-                    <div className='project-display'>
+                    <div className='project-display' id='personal-website-display' onClick={() => console.log('Personal')}>
                         <div className='sprite project-icon' id='personal-website-icon' />
                         <div className='project-text'>Personal Site</div>
                     </div>
                     {/* <div className='sprite project-icon' id='mystery-icon'/> */}
                 </div>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id ornare velit. Donec mollis vitae turpis non laoreet. Maecenas ac mauris non ante egestas tempus. Donec nisl enim, elementum non mauris sed, cursus dictum nisl. Nunc sed tortor pellentesque, elementum nulla id, ullamcorper urna. Curabitur nisi sem, tristique sed velit porta, molestie vulputate sem. Cras non tincidunt sapien. Nunc rhoncus tempus risus, at ornare sem interdum at. Donec rhoncus tortor justo. Praesent dignissim turpis pretium, volutpat sapien vitae, dapibus velit.
+                <div id='project-info-content'>
+                    {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id ornare velit. Donec mollis vitae turpis non laoreet. Maecenas ac mauris non ante egestas tempus. Donec nisl enim, elementum non mauris sed, cursus dictum nisl. Nunc sed tortor pellentesque, elementum nulla id, ullamcorper urna. Curabitur nisi sem, tristique sed velit porta, molestie vulputate sem. Cras non tincidunt sapien. Nunc rhoncus tempus risus, at ornare sem interdum at. Donec rhoncus tortor justo. Praesent dignissim turpis pretium, volutpat sapien vitae, dapibus velit. */}
+                </div>
             </div>
         );
     }
