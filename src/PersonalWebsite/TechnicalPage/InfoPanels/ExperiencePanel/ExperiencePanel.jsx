c8e099db4c5aa8abd7b3096a2b1f37cfcae6aad7
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-08-23 11:11:47 -0500

    Drafted out what the expereince title cards will look like

--- src/PersonalWebsite/TechnicalPage/InfoPanels/ExperiencePanel/ExperiencePanel.jsx
+++ src/PersonalWebsite/TechnicalPage/InfoPanels/ExperiencePanel/ExperiencePanel.jsx
@@ -6,13 +6,22 @@ export default class ExperiencePanel extends React.Component {
     render() {
         return (
             <div className='info-panel-content' id='experience-panel-content'>
-                <hr></hr>
-                <div id='uwf-logo'></div>
-                <div id='ihmc-logo'></div>
-                <div id='panel-text'>
-                    {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id ornare velit. Donec mollis vitae turpis non laoreet. Maecenas ac mauris non ante egestas tempus. Okay */}
+                {/* <hr></hr> */}
+                <div className='exp-container'>
+                    <div id='uwf-logo'></div>
+                    <div className='exp-summary'> 
+                        <div className='exp-title'>University Of West Florida</div>
+                        <div className='exp-suptitle'>This that 2101-1293</div>
+                    </div>
                 </div>
-                <hr></hr>
+                <div className='exp-container'>
+                    <div id='ihmc-logo'></div>
+                    <div className='exp-summary'> 
+                        <div className='exp-title'>IHMC</div>
+                        <div className='exp-suptitle'>This that 2101-1293</div>
+                    </div>
+                </div>
+                {/* <hr></hr> */}
             </div>
         );
     }
