53f519577a6b801fae7631867892b8d5f2b0794d
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-08-23 20:07:44 -0500

    Improved experience card styling

--- src/PersonalWebsite/TechnicalPage/InfoPanels/ExperiencePanel/ExperiencePanel.jsx
+++ src/PersonalWebsite/TechnicalPage/InfoPanels/ExperiencePanel/ExperiencePanel.jsx
@@ -8,18 +8,22 @@ export default class ExperiencePanel extends React.Component {
             <div className='info-panel-content' id='experience-panel-content'>
                 {/* <hr></hr> */}
                 <div className='exp-container'>
+                    <div className='left-border'></div>
                     <div id='uwf-logo'></div>
                     <div className='exp-summary'> 
                         <div className='exp-title'>University Of West Florida</div>
-                        <div className='exp-suptitle'>This that 2101-1293</div>
+                        <div className='exp-subtitle'>B.S. Computer Science<br></br>2020 - 2023</div>
                     </div>
+                    <div className='right-border'></div>
                 </div>
                 <div className='exp-container'>
+                    <div className='left-border'></div>
                     <div id='ihmc-logo'></div>
                     <div className='exp-summary'> 
                         <div className='exp-title'>IHMC</div>
-                        <div className='exp-suptitle'>This that 2101-1293</div>
+                        <div className='exp-subtitle'>Software Engineer Intern<br></br>Summer 2022 - Present</div>
                     </div>
+                    <div className='right-border'></div>
                 </div>
                 {/* <hr></hr> */}
             </div>
