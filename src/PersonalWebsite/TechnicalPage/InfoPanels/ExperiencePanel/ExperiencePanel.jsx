0da6954fa66c00a9c24f3dc1a54d82531a7aa3df
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-08-28 00:02:30 -0500

    Added experience card selection and hover updates

--- src/PersonalWebsite/TechnicalPage/InfoPanels/ExperiencePanel/ExperiencePanel.jsx
+++ src/PersonalWebsite/TechnicalPage/InfoPanels/ExperiencePanel/ExperiencePanel.jsx
@@ -1,14 +1,58 @@
 import React from 'react';
 import './ExperiencePanel.css';
 
+var selectedCard = ''
+
 export default class ExperiencePanel extends React.Component {
+
+    componentDidMount() {
+        selectedCard = 'ihmc'
+        let card = document.getElementById(`${selectedCard}-card`)
+        card.style.marginLeft = '29px'
+    }
+
+    expCardOnClick(type) {
+        console.log(`${type} card clicked`)
+        this.selectCard(type)
+    }
+
+    expCardOnHover(type) {
+        if(type !== selectedCard) {
+            let card = document.getElementById(`${selectedCard}-card`)
+            card.style.marginLeft = '4px'
+            card = document.getElementById(`${type}-card`)
+            card.style.marginLeft = '29px'
+        }
+        console.log(`${type} card hovered`)
+    }
+
+    expCardOnLeave(type) {
+        if(type !== selectedCard) {
+            let card = document.getElementById(`${selectedCard}-card`)
+            card.style.marginLeft = '29px'
+            card = document.getElementById(`${type}-card`)
+            card.style.marginLeft = '4px'
+        }
+    }
+
+    selectCard(type) {
+        let card = document.getElementById(`${type}-card`)
+        card.style.marginLeft = '29px'
+        selectedCard = type
+        document.getElementById('exp-info-section').textContent = type
+    }
 
     render() {
         return (
             <div className='info-panel-content' id='experience-panel-content'>
                 {/* <hr></hr> */}
                 <div id='exp-title-cards'>
-                    <div className='exp-container'>
+                    <div
+                        className='exp-container'
+                        id='uwf-card' onClick={() => this.expCardOnClick('uwf')}
+                        onMouseOver={() => this.expCardOnHover('uwf')}
+                        onMouseLeave={() => this.expCardOnLeave('uwf')}
+                    >
                         <div className='title-card-filler'></div>
                         <div className='left-border'></div>
                         <div id='uwf-logo'></div>
@@ -18,7 +62,12 @@ export default class ExperiencePanel extends React.Component {
                         </div>
                         <div className='right-border'></div>
                     </div>
-                    <div className='exp-container'>
+                    <div 
+                        className='exp-container'
+                        id='ihmc-card' onClick={() => this.expCardOnClick('ihmc')}
+                        onMouseOver={() => this.expCardOnHover('ihmc')}
+                        onMouseLeave={() => this.expCardOnLeave('ihmc')}
+                    >
                         <div className='title-card-filler'></div>
                         <div className='left-border'></div>
                         <div id='ihmc-logo'></div>
@@ -28,9 +77,25 @@ export default class ExperiencePanel extends React.Component {
                         </div>
                         <div className='right-border'></div>
                     </div>
+                    {/* <div
+                        className='exp-container'
+                        id='other-card' onClick={() => this.expCardOnClick('other')}
+                        onMouseOver={() => this.expCardOnHover('other')}
+                        onMouseLeave={() => this.expCardOnLeave('other')}
+                    >
+                        <div className='title-card-filler'></div>
+                        <div className='left-border'></div>
+                        <div id='uwf-logo'></div>
+                        <div className='exp-summary'> 
+                            <div className='exp-title'>University Of West Florida</div>
+                            <div className='exp-subtitle'>B.S. Computer Science<br></br>2020 - 2023</div>
+                        </div>
+                        <div className='right-border'></div>
+                    </div> */}
                 </div>
                 <div id='divider'></div>
                 <div id='exp-info-section'>
+                    Hello lets throw some text here
                 </div>
                 {/* <hr></hr> */}
             </div>
