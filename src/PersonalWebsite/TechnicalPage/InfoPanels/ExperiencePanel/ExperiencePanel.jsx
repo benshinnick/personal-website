e08d4cb2b1a6b9f332b03f23b2d77aab2a4dfe41
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-09-03 12:26:59 -0500

    Implemented experience panel description content styling

--- src/PersonalWebsite/TechnicalPage/InfoPanels/ExperiencePanel/ExperiencePanel.jsx
+++ src/PersonalWebsite/TechnicalPage/InfoPanels/ExperiencePanel/ExperiencePanel.jsx
@@ -33,7 +33,24 @@ export default class ExperiencePanel extends React.Component {
         card.style.marginLeft = '16px'
         selectedCard = type
 
-        document.getElementById('exp-info-section').textContent = type
+        let title = ''
+        let description = ''
+        let container = document.getElementById('exp-info-section')
+        if(type === 'uwf') {
+            title = 'University of West Florida'
+            description = 'Concentation in Software Engineering. More about my time at UWF and what I did. More text to fill space. I would like 3-5 sentences here explaining stuff. Maybe timelines or other stuff?'
+            container.style.backgroundColor = 'rgba(21, 144, 206, 0.075)'
+            container.style.borderColor = 'rgba(21, 144, 206, 0.75)'
+        }
+        if(type === 'ihmc') {
+            title = 'Institute of Human and Machine Cognition'
+            description = 'More about my time at IHMC and what I did, I have a lot to say, but just want placeholder text here now. I will do other stuff later.'
+            container.style.backgroundColor = 'rgba(0, 0, 0, 0.05)'
+            container.style.borderColor = 'rgb(0, 0, 0)'
+        }
+
+        document.getElementById('title-text').textContent = title
+        document.getElementById('description-container').textContent = description
     }
 
     render() {
@@ -51,7 +68,7 @@ export default class ExperiencePanel extends React.Component {
                         <div className='left-border'></div>
                         <div id='uwf-logo'></div>
                         <div className='exp-summary'> 
-                            <div className='exp-title'>University Of West Florida</div>
+                            <div className='exp-title'>University of West Florida</div>
                             <div className='exp-subtitle'>B.S. Computer Science<br></br>2020 - 2023</div>
                         </div>
                         <div className='right-border'></div>
@@ -88,7 +105,10 @@ export default class ExperiencePanel extends React.Component {
                     </div> */}
                 </div>
                 <div id='divider'></div>
-                <div id='exp-info-section'></div>
+                <div id='exp-info-section'>
+                    <div id='title-text'/>
+                    <div id='description-container'/>
+                </div>
                 {/* <hr></hr> */}
             </div>
         );
