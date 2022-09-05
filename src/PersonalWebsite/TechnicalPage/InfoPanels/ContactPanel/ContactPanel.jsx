712d822684d64e0c5ffb9e1eb7b77b6137636a78
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-09-05 14:46:48 -0500

    Finished adding contact panel content

--- src/PersonalWebsite/TechnicalPage/InfoPanels/ContactPanel/ContactPanel.jsx
+++ src/PersonalWebsite/TechnicalPage/InfoPanels/ContactPanel/ContactPanel.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './ContactPanel.css';
+import ConnectPanel from '../../../HomePage/InfoPanels/ConnectPanel/ConnectPanel';
 
 export default class ContactPanel extends React.Component {
 
@@ -10,6 +11,14 @@ export default class ContactPanel extends React.Component {
                 <div id='panel-text'>
                     I would be happy to further discuss my experiences with you, simply shoot me an email or message me on LinkedIn!
                 </div>
+                <hr></hr>
+                <div id='email-line'>
+                    <div className='email-icons' id='left-icons'>E F N</div>
+                    <div id='email-text'>Email: shinnickbenjamin@gmail</div>
+                    <div className='email-icons' id='right-icons'>a s W</div>
+                </div>
+                <hr></hr>
+                <ConnectPanel />
                 <hr></hr>
             </div>
         );
