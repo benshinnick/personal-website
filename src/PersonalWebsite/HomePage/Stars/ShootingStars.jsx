94adfd636d7bd0bafaeb28a4dbc5630452b89d34
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-09-11 20:44:34 -0500

    Added shooting star image and boilerplate for home page

---
+++ src/PersonalWebsite/HomePage/Stars/ShootingStars.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Stars.css';
+
+export default class ShootingStars extends React.Component {
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div id='stars'></div>
+        );
+    }
+}
