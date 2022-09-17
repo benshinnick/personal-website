14ba9243ede4cdf79e16d9f953b33052ae2a66f2
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-09-16 23:05:45 -0500

    Implemented shooting stars random intervals and spawn locations

--- src/PersonalWebsite/HomePage/Stars/ShootingStars.jsx
+++ src/PersonalWebsite/HomePage/Stars/ShootingStars.jsx
@@ -3,12 +3,31 @@ import './ShootingStars.css';
 
 const SPRITE_SHEET_WIDTH = 1648
 const NUM_FRAMES = 4;
-const ANIMATION_SPEED_MS =  100
+const ANIMATION_SPEED_MS =  50
 
 export default class ShootingStars extends React.Component {
 
     componentDidMount() {
-        this.startAnimation(document.getElementById('shooting-star-1'))
+        this.shootStars()
+    }
+    
+    shootStars() {
+        setInterval(() => {
+            if (Math.random() < 0.65) {
+                let newStar = document.createElement("div");
+                newStar.classList.add('shooting-star')
+                if (Math.random() < 0.5) {
+                    newStar.style.top = `${randomIntFromInterval(-140, window.innerHeight)}px`
+                    newStar.style.right = `${-412}px`
+                } else {
+                    newStar.style.top = `${-140}px`
+                    newStar.style.right = `${randomIntFromInterval(-412, window.innerWidth)}px`
+                }
+                document.getElementById('shooting-stars').appendChild(newStar)
+                this.startAnimation(newStar)
+                setTimeout(() => { newStar.remove() }, 2000)
+            }
+        }, 650)
     }
 
     startAnimation(star) {
@@ -26,9 +45,11 @@ export default class ShootingStars extends React.Component {
 
     render() {
         return (
-            <div id='shooting-stars'>
-                <div id='shooting-star-1' className='shooting-star sprite'></div>
-            </div>
+            <div id='shooting-stars'></div>
         );
     }
+}
+
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
