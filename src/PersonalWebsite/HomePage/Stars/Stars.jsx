e0d98a46836e792540386824ec896ad5952ecc3c
Ben Shinnick <shinnickbenjamin@gmail.com>
2023-05-09 21:10:06 -0500

    Home page is now fully responsive

--- src/PersonalWebsite/HomePage/Stars/Stars.jsx
+++ src/PersonalWebsite/HomePage/Stars/Stars.jsx
@@ -3,7 +3,7 @@ import './Stars.css';
 
 const NUM_FRAMES = 12;
 const ANIMATION_SPEED_MS = 750
-const NUM_STARS = 7
+const NUM_STARS = 9
 
 export default class Stars extends React.Component {
 
@@ -29,13 +29,15 @@ export default class Stars extends React.Component {
     }
 
     getSpriteSheetWidth(starNum) {
-        if(starNum === 1) return 900
+        if (starNum === 1) return 900
         else if (starNum === 2) return 804
         else if (starNum === 3) return 1068
         else if (starNum === 4) return 528
         else if (starNum === 5) return 816
         else if (starNum === 6) return 720
         else if (starNum === 7) return 528
+        else if (starNum === 8) return 900
+        else if (starNum === 9) return 528
     }
 
     render() {
@@ -48,6 +50,8 @@ export default class Stars extends React.Component {
                 <div className='sprite star' id='star-5'></div>
                 <div className='sprite star' id='star-6'></div>
                 <div className='sprite star' id='star-7'></div>
+                <div className='sprite star' id='star-8'></div>
+                <div className='sprite star' id='star-9'></div>
             </div>
         );
     }
