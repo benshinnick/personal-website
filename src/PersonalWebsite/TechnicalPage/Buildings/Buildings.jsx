f78e08c499122350375af413fcebf1298c16dd6c
Ben Shinnick <shinnickbenjamin@gmail.com>
2023-05-11 20:21:17 -0500

    Responsive updates to buildings

--- src/PersonalWebsite/TechnicalPage/Buildings/Buildings.jsx
+++ src/PersonalWebsite/TechnicalPage/Buildings/Buildings.jsx
@@ -8,20 +8,39 @@ export default class Buildings extends React.Component {
     }
 
     onScroll(scrollY) {
+        const screenWidth = window.innerWidth
         const left1 = document.getElementById('left-border-building-1')
-        left1.style.marginTop = `${Math.floor(-scrollY/6 + 550)}px`
+        if(screenWidth > 650) left1.style.marginTop = `${Math.floor(-scrollY/6 + 550)}px`
+        else left1.style.marginTop = `${Math.floor(-scrollY/26 + 550)}px`
 
         const left2 = document.getElementById('left-border-building-2')
-        left2.style.marginTop = `${Math.floor(-scrollY/16 + 600)}px`
-        
+        if(screenWidth > 650) left2.style.marginTop = `${Math.floor(-scrollY/20 + 600)}px`
+        else left2.style.marginTop = `${Math.floor(-scrollY/36 + 600)}px`
+
         const right1 = document.getElementById('right-border-building-1')
-        right1.style.marginTop = `${Math.floor(-scrollY/14 + 650)}px`
+        if(screenWidth > 650) right1.style.marginTop = `${Math.floor(-scrollY/14 + 650)}px`
+        else right1.style.marginTop = `${Math.floor(-scrollY/34 + 650)}px`
 
         const right2 = document.getElementById('right-border-building-2')
-        right2.style.marginTop = `${Math.floor(-scrollY/8 + 650)}px`
+        if(screenWidth > 650) right2.style.marginTop = `${Math.floor(-scrollY/8 + 650)}px`
+        else right2.style.marginTop = `${Math.floor(-scrollY/28 + 650)}px`
 
         const right3 = document.getElementById('right-border-building-3')
-        right3.style.marginTop = `${Math.floor(-scrollY/18 + 500)}px`
+        if(screenWidth > 650) right3.style.marginTop = `${Math.floor(-scrollY/18 + 500)}px`
+        else right3.style.marginTop = `${Math.floor(-scrollY/38 + 500)}px`
+
+        // outer buildings
+        const left3 = document.getElementById('left-border-building-3')
+        left3.style.marginTop = `${Math.floor(-scrollY/14 + 250)}px`
+        const left4 = document.getElementById('left-border-building-4')
+        left4.style.marginTop = `${Math.floor(-scrollY/18 + 400)}px`
+        const left5 = document.getElementById('left-border-building-5')
+        left5.style.marginTop = `${Math.floor(-scrollY/16 + 550)}px`
+
+        const right4 = document.getElementById('right-border-building-4')
+        right4.style.marginTop = `${Math.floor(-scrollY/16 + 400)}px`
+        const right5 = document.getElementById('right-border-building-5')
+        right5.style.marginTop = `${Math.floor(-scrollY/12 + 700)}px`
     }
 
     render() {
@@ -33,22 +52,42 @@ export default class Buildings extends React.Component {
                         <div className='building-4-floors sprite'></div>
                     </div>
                     <div id='left-border-building-2'>
-                            <div id='building-2-top' className='sprite'></div>
-                            <div id='building-2-floors' className='sprite'></div>
+                        <div className='building-2-top sprite'></div>
+                        <div className='building-2-floors sprite'></div>
+                    </div>
+                    <div id='left-border-building-3'>
+                        <div className='building-2-top sprite'></div>
+                        <div className='building-2-floors sprite'></div>
+                    </div>
+                    <div id='left-border-building-4'>
+                        <div className='building-2-top sprite'></div>
+                        <div className='building-2-floors sprite'></div>
+                    </div>
+                    <div id='left-border-building-5'>
+                        <div className='building-1-top sprite'></div>
+                        <div className='building-1-floors sprite'></div>
                     </div>
                 </div>
                 <div id='right-border-buildings'>
                     <div id='right-border-building-1'>
-                        <div id='building-1-top' className='sprite'></div>
-                        <div id='building-1-floors' className='sprite'></div>
+                        <div className='building-1-top sprite'></div>
+                        <div className='building-1-floors sprite'></div>
                     </div>
                     <div id='right-border-building-2'>
-                            <div id='building-3-top' className='sprite'></div>
-                            <div id='building-3-floors' className='sprite'></div>
+                        <div className='building-3-top sprite'></div>
+                        <div className='building-3-floors sprite'></div>
                     </div>
                     <div id='right-border-building-3'>
-                            <div id='building-1-top' className='sprite'></div>
-                            <div id='building-5-floors' className='sprite'></div>
+                        <div className='building-1-top sprite'></div>
+                        <div className='building-5-floors sprite'></div>
+                    </div>
+                    <div id='right-border-building-4'>
+                        <div className='building-2-top sprite'></div>
+                        <div className='building-2-floors sprite'></div>
+                    </div>
+                    <div id='right-border-building-5'>
+                        <div className='building-2-top sprite'></div>
+                        <div className='building-2-floors sprite'></div>
                     </div>
                 </div>
             </div>
