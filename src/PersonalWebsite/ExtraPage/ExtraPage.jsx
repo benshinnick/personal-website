18be7a847a5b134462d0a8b298605f5081156e98
Ben Shinnick <shinnickbenjamin@gmail.com>
2023-06-25 15:32:27 -0500

    Added tilt effect to game carts

--- src/PersonalWebsite/ExtraPage/ExtraPage.jsx
+++ src/PersonalWebsite/ExtraPage/ExtraPage.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import './ExtraPage.css';
+import * as vt from './vanilla-tilt.js';
 
 export default class TechnicalPage extends React.Component {
     componentDidMount() {
+        vt.VanillaTilt.init(document.querySelector("#snake-game-selection"), {
+            max: 14,
+            speed: 8
+        });
+        vt.VanillaTilt.init(document.querySelector("#tetris-game-selection"), {
+            max: 18,
+            speed: 10
+        });
+        vt.VanillaTilt.init(document.querySelector("#minesweeper-game-selection"), {
+            max: 18,
+            speed: 10
+        });
     }
 
     componentWillUnmount() {
@@ -14,15 +27,15 @@ export default class TechnicalPage extends React.Component {
                 <div className='fill'>
                     <div id='computer-layout' className='sprite'/>
                     <div id='game-selection-container'>
-                        <div className='game-selection'>
+                        <div className='game-selection' id='snake-game-selection'>
                             <div id='snake-game-cart' className='sprite'></div>
                             <div className='game-selection-text'>SNAKE</div>
                         </div>
-                        <div className='game-selection'>
+                        <div className='game-selection' id='tetris-game-selection'>
                             <div id='tetris-game-cart' className='sprite'></div>
                             <div className='game-selection-text'>TETRIS</div>
                         </div>
-                        <div className='game-selection'>
+                        <div className='game-selection' id='minesweeper-game-selection'>
                             <div id='minesweeper-game-cart' className='sprite'></div>
                             <div className='game-selection-text'>MINESWEEPER</div>
                         </div>
