e3f8c0b0ccbd3ea1697e9a0044ae6c3d886e4de6
Ben Shinnick <shinnickbenjamin@gmail.com>
2023-07-02 23:03:06 -0400

    Computer screen pages are now dynamically loaded

--- src/PersonalWebsite/ExtraPage/ExtraPage.jsx
+++ src/PersonalWebsite/ExtraPage/ExtraPage.jsx
@@ -9,7 +9,7 @@ export default class TechnicalPage extends React.Component {
             max: 14,
             speed: 8
         });
-        vt.VanillaTilt.init(document.querySelector("#tetris-game-selection"), {
+        vt.VanillaTilt.init(document.querySelector("#tetra-mix-game-selection"), {
             max: 14,
             speed: 8
         });
@@ -17,10 +17,122 @@ export default class TechnicalPage extends React.Component {
             max: 14,
             speed: 8
         });
+        this.loadPasswordScreen()
     }
 
     // componentWillUnmount() {
     // }
+
+    clearComputerScreen() {
+        document.getElementById('computer-screen').innerHTML = '';
+    }
+
+    loadPasswordScreen() {
+        this.clearComputerScreen();
+
+        var passwordInputContainer = document.createElement('div')
+        passwordInputContainer.id = 'password-input-container'
+
+        var passwordLabel = document.createElement('div')
+        passwordLabel.id = 'password-label'
+        passwordLabel.innerHTML = 'Password: '
+        passwordInputContainer.appendChild(passwordLabel)
+
+        var passwordInput = document.createElement('input')
+        passwordInput.id = 'password-input'
+        passwordInput.type = 'text'
+        passwordInput.name = 'password'
+        passwordInput.autocomplete = 'off'
+        passwordInputContainer.appendChild(passwordInput)
+
+        var passwordSubmitButton = document.createElement('div')
+        passwordSubmitButton.id = 'password-submit-button'
+        passwordSubmitButton.addEventListener("click", (event) => { this.passwordSubmitButtonOnClick(); });
+
+        var topLeftCorner = document.createElement('div')
+        topLeftCorner.className = 'corner'
+        topLeftCorner.id = 'submit-top-left-corner'
+        var topRightCorner = document.createElement('div')
+        topRightCorner.className = 'corner'
+        topRightCorner.id = 'submit-top-right-corner'
+        var bottomRightCorner = document.createElement('div')
+        bottomRightCorner.className = 'corner'
+        bottomRightCorner.id = 'submit-bottom-right-corner'
+        var bottomLeftCorner = document.createElement('div')
+        bottomLeftCorner.className = 'corner'
+        bottomLeftCorner.id = 'submit-bottom-left-corner'
+        passwordSubmitButton.appendChild(topLeftCorner)
+        passwordSubmitButton.appendChild(topRightCorner)
+        passwordSubmitButton.appendChild(bottomRightCorner)
+        passwordSubmitButton.appendChild(bottomLeftCorner)
+
+        var topBorder = document.createElement('div')
+        topBorder.className = 'submit-border'
+        topBorder.id = 'submit-top-border'
+        passwordSubmitButton.appendChild(topBorder)
+
+        var enterInput = document.createElement('input')
+        enterInput.type = 'submit'
+        enterInput.id = 'password-submit-input'
+        enterInput.value = 'Enter'
+        passwordSubmitButton.appendChild(enterInput)
+
+        var bottomBorder = document.createElement('div')
+        bottomBorder.className = 'submit-border'
+        bottomBorder.id = 'submit-bottom-border'
+        passwordSubmitButton.appendChild(bottomBorder)
+
+        passwordInputContainer.appendChild(passwordSubmitButton)
+
+        var passwordClue = document.createElement('div')
+        passwordClue.id = 'password-clue'
+        passwordClue.innerHTML = 'Password Sections Unlocked (0/3)'
+        passwordInputContainer.appendChild(passwordClue)
+
+        document.getElementById('computer-screen').appendChild(passwordInputContainer)
+    }
+
+    loadSnakeTitleScreen() {
+        this.clearComputerScreen();
+
+        var snakeTitleScreenContainer = document.createElement('div')
+        snakeTitleScreenContainer.id = 'password-input-container'
+
+        var titleLabel = document.createElement('div')
+        titleLabel.id = 'password-label'
+        titleLabel.innerHTML = 'SNAKE'
+        snakeTitleScreenContainer.appendChild(titleLabel)
+
+        document.getElementById('computer-screen').appendChild(snakeTitleScreenContainer)
+    }
+
+    loadTetraMixTitleScreen() {
+        this.clearComputerScreen();
+
+        var tetraMixTitleScreenContainer = document.createElement('div')
+        tetraMixTitleScreenContainer.id = 'password-input-container'
+
+        var titleLabel = document.createElement('div')
+        titleLabel.id = 'password-label'
+        titleLabel.innerHTML = 'TETRA MIX'
+        tetraMixTitleScreenContainer.appendChild(titleLabel)
+
+        document.getElementById('computer-screen').appendChild(tetraMixTitleScreenContainer)
+    }
+
+    loadMinesweeperTitleScreen() {
+        this.clearComputerScreen();
+
+        var minesweeperTitleScreenContainer = document.createElement('div')
+        minesweeperTitleScreenContainer.id = 'password-input-container'
+
+        var titleLabel = document.createElement('div')
+        titleLabel.id = 'password-label'
+        titleLabel.innerHTML = 'MINESWEEPER'
+        minesweeperTitleScreenContainer.appendChild(titleLabel)
+
+        document.getElementById('computer-screen').appendChild(minesweeperTitleScreenContainer)
+    }
 
     passwordSubmitButtonOnClick() {
         console.log("password submit button was clicked")
@@ -32,14 +144,17 @@ export default class TechnicalPage extends React.Component {
 
     snakeGameSelected() {
         console.log("snake game was selected")
+        this.loadSnakeTitleScreen()
     }
 
-    tetrisGameSelected() {
-        console.log("tetris game was selected")
+    tetraMixGameSelected() {
+        console.log("tetra mix game was selected")
+        this.loadTetraMixTitleScreen()
     }
 
     minesweeperGameSelected() {
         console.log("minesweeper game was selected")
+        this.loadMinesweeperTitleScreen()
     }
 
     render() {
@@ -48,31 +163,16 @@ export default class TechnicalPage extends React.Component {
                 <div className='fill'>
                 <div id='nav-bar-background'></div>
                     <div id='computer-layout' className='sprite'>
-                        <div id='computer-screen'>
-                            <div id="password-input-container">
-                                <div id="password-label">Password: </div>
-                                <input type="text" id="password-input" name="password" autoComplete="off"></input>
-                                <div id="password-submit-button" onClick={() => this.passwordSubmitButtonOnClick()}>
-                                    <div className="corner" id="submit-top-left-corner"></div>
-                                    <div className="corner" id="submit-top-right-corner"></div>
-                                    <div className="corner" id="submit-bottom-right-corner"></div>
-                                    <div className="corner" id="submit-bottom-left-corner"></div>
-                                    <div className="submit-border" id="submit-top-border"></div>
-                                    <input type="submit" id="password-submit-input" value="Enter"></input>
-                                    <div className="submit-border" id="submit-bottom-border"></div>
-                                </div>
-                                <div id='password-clue'>Password Sections Unlocked (0/3)</div>
-                            </div>
-                        </div>
+                        <div id='computer-screen'></div>
                     </div>
                     <div id='game-selection-container'>
                         <div className='game-selection' id='snake-game-selection' onClick={() => this.snakeGameSelected()}>
                             <div id='snake-game-cart' className='sprite'></div>
                             <div id='snake-label' className='game-selection-text'>SNAKE</div>
                         </div>
-                        <div className='game-selection' id='tetris-game-selection' onClick={() => this.tetrisGameSelected()}>
-                            <div id='tetris-game-cart' className='sprite'></div>
-                            <div id='tetris-label' className='game-selection-text'>TETRIS</div>
+                        <div className='game-selection' id='tetra-mix-game-selection' onClick={() => this.tetraMixGameSelected()}>
+                            <div id='tetra-mix-game-cart' className='sprite'></div>
+                            <div id='tetra-mix-label' className='game-selection-text'>TETRA MIX</div>
                         </div>
                         <div className='game-selection' id='minesweeper-game-selection' onClick={() => this.minesweeperGameSelected()}>
                             <div id='minesweeper-game-cart' className='sprite'></div>
