eb3aaa31fc8f0d2a08c88a4eb9b56ce56a25d3ca
Ben Shinnick <shinnickbenjamin@gmail.com>
2023-08-05 14:14:26 -0500

    Added all snake game sprites needed to play the game

--- src/PersonalWebsite/ExtraPage/SnakeGame/Position.js
+++ src/PersonalWebsite/ExtraPage/SnakeGame/Position.js
@@ -1,6 +1,7 @@
 export class Position {
-    constructor(x, y) {
+    constructor(x, y, type) {
         this.x = x;
         this.y = y;
+        this.type = type; // tail-dir, body-1/2, corner-dir, head-dir, food, wall?
     }
 }
